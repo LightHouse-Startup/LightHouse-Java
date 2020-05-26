7e6838a6edc16179b60af3ce7beafadf9ba47ca4
xiecf <xcf_love@live.cn>
2020-05-26 16:57:29 +0800

    Create HelloServiceAutoConfiguration.java

---
+++ rest-starter/src/main/java/light/house/rest/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,26 @@
+package light.house.rest.starter;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HelloProperties.class)
+@ConditionalOnClass(HelloService.class)
+public class HelloServiceAutoConfiguration {
+
+    private final HelloProperties helloProperties;
+
+    public HelloServiceAutoConfiguration(HelloProperties helloProperties) {
+        this.helloProperties = helloProperties;
+    }
+
+    @Bean
+    HelloService helloService() {
+        HelloService helloService = new HelloService();
+        helloService.setName(helloProperties.getName());
+        helloService.setMsg(helloProperties.getMsg());
+        return helloService;
+    }
+}
