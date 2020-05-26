c78d4e9abc69a588ad968df549cb5c038ff12088
xiecf <xcf_love@live.cn>
2020-05-26 17:12:10 +0800

    Create HelloController.java

---
+++ book/src/main/java/light/house/book/controller/HelloController.java
@@ -0,0 +1,20 @@
+package light.house.book.controller;
+
+import light.house.rest.starter.HelloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return helloService.sayHello();
+    }
+}
