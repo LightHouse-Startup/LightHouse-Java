cd3b38de1a1a91b67c8595babbfd928a6f5fc931
xiecf <xcf_love@live.cn>
2020-05-26 17:07:46 +0800

    Update HelloProperties.java

--- rest-starter/src/main/java/light/house/rest/starter/HelloProperties.java
+++ rest-starter/src/main/java/light/house/rest/starter/HelloProperties.java
@@ -2,22 +2,27 @@ package light.house.rest.starter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "javaboy")
+@ConfigurationProperties(prefix = "welcome")
 public class HelloProperties {
-    private static final String DEFAULT_NAME = "江南一点雨";
-    private static final String DEFAULT_MSG = "牧码小子";
+    private static final String DEFAULT_NAME = "xiecf";
+    private static final String DEFAULT_LANGUAGE = "java";
+
     private String name = DEFAULT_NAME;
-    private String msg = DEFAULT_MSG;
+    private String language = DEFAULT_LANGUAGE;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public String getMsg() {
-        return msg;
+
+    public String getLanguage() {
+        return language;
     }
-    public void setMsg(String msg) {
-        this.msg = msg;
+
+    public void setLanguage(String language) {
+        this.language = language;
     }
 }
