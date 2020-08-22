9f76cfa6ab0993eb4e1848c9ebef3e8a8214ed17
xiecf <xcf_love@live.cn>
2020-08-22 23:45:20 +0800

    'eureka-server'

---
+++ eureka-server/src/main/java/light/house/startup/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package light.house.startup.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/test/java/light/house/startup/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package light.house.startup.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
